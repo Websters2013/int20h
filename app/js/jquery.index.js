4976b758a1351d3258a068de728778d35d8136bd
alexserga <snike1985@gmail.com>
2017-02-22 09:46:16 +0200

    functional doing

--- app/js/jquery.index.js
+++ app/js/jquery.index.js
@@ -1,3 +1,59 @@
 ( function(){
 
+    $(function(){
+
+        $('.site').each( function() {
+            new Site( $(this) );
+        } );
+
+    });
+
+    var Site = function(obj) {
+
+        //private properties
+        var _obj = obj,
+            _topBtn = _obj.find( '.site__top' ),
+            _scrollDuration = 500;
+
+        //private methods
+        var _addEvents = function() {
+
+                _topBtn.on({
+                    'click': function() {
+                        _scrollTop();
+                        return false;
+                    }
+                });
+
+                $( window ).on({
+                    'scroll': function() {
+                        var curElem = $( this ),
+                            winHeight = curElem.height(),
+                            scrollValue = curElem.scrollTop();
+
+                        if ( scrollValue > 2*winHeight ) {
+                            _topBtn.addClass( 'active' );
+                        } else  {
+                            _topBtn.removeClass( 'active' );
+                        }
+                    }
+                });
+
+            },
+            _scrollTop = function() {
+
+                $('html, body').animate({ scrollTop: 0 }, _scrollDuration);
+
+            },
+            _init = function() {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
 } )();
